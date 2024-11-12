c5a3f71a8ac4aa94b2a82ca898912ec5211b974f
KeiThong <fookeithong@gmail.com>
2024-11-12 13:55:07 +0800

    add increment and decrement button

--- src/main/java/com/example/demo/view/SettingsPage.java
+++ src/main/java/com/example/demo/view/SettingsPage.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -31,9 +32,23 @@ public class SettingsPage extends VBox {
         Slider volumeSlider = new Slider(0, 100, 50); // Min: 0, Max: 100, Initial: 50
         volumeSlider.setMinorTickCount(4);
         volumeSlider.setBlockIncrement(10);
-        volumeSlider.setPrefWidth(SCREEN_WIDTH); 
+        volumeSlider.setPrefWidth(50); 
 
-        // // Mute button
+        // // Decrement button
+        Button decrementButton = new Button("-");
+        decrementButton.setStyle("-fx-font-size: 18px;");
+        decrementButton.setOnMouseClicked(event -> volumeSlider.setValue(volumeSlider.getValue() - 5)); // Decrease slider by 5
+        
+        // // Increment button
+        Button incrementButton = new Button("+");
+        incrementButton.setStyle("-fx-font-size: 18px;");
+        incrementButton.setOnMouseClicked(event -> volumeSlider.setValue(volumeSlider.getValue() + 5)); // Toggle mute state on click
+
+        // Horizontal box to hold decrement button, slider, and increment button
+        HBox sliderBox = new HBox(10, decrementButton, volumeSlider, incrementButton);
+        sliderBox.setAlignment(Pos.CENTER);
+
+        // Mute button
         Button muteButton = new Button("Mute");
         muteButton.setStyle("-fx-font-size: 18px;");
         muteButton.setOnMouseClicked(event -> toggleMute(volumeSlider, muteButton)); // Toggle mute state on click
@@ -47,7 +62,7 @@ public class SettingsPage extends VBox {
         this.setAlignment(Pos.CENTER);
         this.setSpacing(20);
 
-        this.getChildren().addAll(title, volumeLabel, volumeSlider, muteButton, backButton);
+        this.getChildren().addAll(title, volumeLabel, volumeSlider, decrementButton, incrementButton, muteButton, backButton);
     }
 
     private void toggleMute(Slider volumeSlider, Button muteButton) {
