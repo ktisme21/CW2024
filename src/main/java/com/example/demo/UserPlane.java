aa75d37b38fdde2c06d71eb485330e26215f49c1
KeiThong <fookeithong@gmail.com>
2024-11-20 14:45:40 +0800

    resize the alighment of the screen - xleftbound and xrightbound

--- src/main/java/com/example/demo/UserPlane.java
+++ src/main/java/com/example/demo/UserPlane.java
@@ -4,8 +4,8 @@ public class UserPlane extends FighterPlane {
 
 	private static final String IMAGE_NAME = "userplane.png";
 	// Allow userplane move horizontally
-	private static final double X_LEFT_BOUND = 0;
-	private static final double X_RIGHT_BOUND = 1150;
+	private static final double X_LEFT_BOUND = -30;
+	private static final double X_RIGHT_BOUND = 1110;
 	private static final double Y_UPPER_BOUND = -40;
 	private static final double Y_LOWER_BOUND = 600.0;
 	private static final double INITIAL_X_POSITION = 5.0;
